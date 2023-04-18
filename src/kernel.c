28f4b289b6b69f3db35e019f32be2b22cdfaa7a6
Itay Hadad <itay5245@gmail.com>
2023-04-18 21:40:35 +0300

    finished vga buffer with new line handle

--- src/kernel.c
+++ src/kernel.c
@@ -57,8 +57,12 @@ size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
 
-void terminal_initialize(void)
-{
+void handle_new_line(void) {
+    terminal_row++;
+    terminal_column = 0;
+}
+
+void terminal_initialize(void) {
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
@@ -72,8 +76,49 @@ void terminal_initialize(void)
 	}
 }
 
+void terminal_setcolor(uint8_t color) {
+	terminal_color = color;
+}
+
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = vga_entry(c, color);
+}
+
+void handle_filled_vga_height(void) {
+	terminal_row = 0;
+}
+
+void terminal_putchar(char c) {
+    if(c == '\n') {
+        handle_new_line();
+    } else {
+        terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+        if (++terminal_column == VGA_WIDTH) {
+		    terminal_column = 0;
+            terminal_row++;
+	    }
+    }
+
+    if (terminal_row == VGA_HEIGHT) {
+        handle_filled_vga_height();
+    }
+}
+
+void terminal_write(const char* data, size_t size)  {
+	for (size_t i = 0; i < size; i++)
+		terminal_putchar(data[i]);
+}
+
+void terminal_write_string(const char* data) {
+	terminal_write(data, strlen(data));
+}
+
 void kernel_main(void) {
     terminal_initialize();
+
+    terminal_write_string("Hello, kernel World!\n");
+    terminal_write_string("newline!\n");
 
     while(1) {}
 }

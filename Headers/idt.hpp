7bde1747eaea2d2420f4c465952bdc36915034e9
Itay Hadad <itay5245@gmail.com>
2023-06-19 21:14:00 +0300

    rename and fix to idt entry struct

--- Headers/idt.hpp
+++ Headers/idt.hpp
@@ -2,24 +2,24 @@
 
 #include <stdint.h>
 
-typedef struct {
-    uint16_t offset_1;       // offset bits 0..15
+struct idt_entry_64 {
+    uint16_t offset_low;       // offset bits 0..15
     uint16_t selector;       // a code segment selector in GDT or LDT
     uint8_t ist;             // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
     uint8_t type_attributes; // gate type, dpl, and p fields
-    uint16_t offset_2;       // offset bits 16..31
-    uint32_t offset_3;       // offset bits 32..63
+    uint16_t offset_mid;       // offset bits 16..31
+    uint32_t offset_high;       // offset bits 32..63
     uint32_t zero;           // reserved
-} __attribute__((packed)) interrupt_descriptor_64;
+} __attribute__((packed));
 
 #define IDT_64BIT_INTERRUPT_GATE 0xE
 #define IDT_STORAGE_SEGMENT 0x20
 #define IDT_DPL_3 0x60
 #define IDT_PRESENT 0x80
 
-#define INT_END asm volatile("iret");
+#define INT_END asm volatile("iretq");
 
 extern void idt_init();
 extern void exceptions_init();
 extern void idt_register_interrupt(uint8_t i, uint32_t addr);
-extern void add_idt_descriptor(uint8_t id, interrupt_descriptor_64 desc);
+extern void add_idt_descriptor(uint8_t id, idt_entry_64 desc);

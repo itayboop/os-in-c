8987132e5a2e5d8a4234f75a83ca6f5b853871a7
Itay Hadad <itay5245@gmail.com>
2023-07-12 09:13:08 +0300

    pointer alignment

--- Sources/arch/interrupts/idt.cpp
+++ Sources/arch/interrupts/idt.cpp
@@ -14,7 +14,7 @@ void register_interrupt_handler(uint8_t interrupt_number, Isr handler_func)
 }
 
 extern "C" {
-	Registers* isr_handler(Registers *registers)
+	Registers* isr_handler(Registers* registers)
 	{
 		if (interrupt_handlers[registers->interrupt_number] == 0)
 		{

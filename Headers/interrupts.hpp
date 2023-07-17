d8c320614303ffdb60531096bed55cffddb579d9
Itay Hadad <itay5245@gmail.com>
2023-07-17 22:29:32 +0300

    interrupt codes to enum class

--- Headers/interrupts.hpp
+++ Headers/interrupts.hpp
@@ -1,24 +1,26 @@
 #pragma once
-#include "idt.hpp"
 
-#define EXC_DIV_BY_ZERO 0
-#define EXC_DEBUG 1
-#define EXC_NMI 2
-#define EXC_BP 3
-#define EXC_OVERFLOW 4
-#define EXC_BR 5
-#define EXC_INV_OPCODE 6
-#define EXC_DU 7
-#define EXC_DB_FAULT 8
-#define EXC_INV_TSS 10
-#define EXC_NOT_PRESENT 11
-#define EXC_SSF 12
-#define EXC_GP 13
-#define EXC_PF 14
-#define EXC_MF 16
-#define EXC_AC 17
-#define EXC_MC 18
-#define EXC_XM 19
-#define EXC_VE 20
+enum class InterruptCode : uint8_t
+{
+	DIV_BY_ZERO = 0,
+	DEBUG,
+	NON_MASKABLE_INT,
+	BREAKPOINT,
+	OVERFLOW,
+	BOUND_RANGE,
+	INV_OPCODE,
+	DEVICE_UNAVAILABLE,
+	DOUBLE_FAULT,
+	INV_TSS = 10,
+	NOT_PRESENT,
+	STACK_SEGMANT_FAULT,
+	GENRAL_PROTECTION_FAULT,
+	PAGE_FAULT,
+	KERNEL_FPU = 16,
+	ALIGN_CHECK,
+	MACHINE_CHECK,
+	SIMF_FP,
+	VIRTUALIZATION,
+};
 
 void register_all_interrupt_handlers();

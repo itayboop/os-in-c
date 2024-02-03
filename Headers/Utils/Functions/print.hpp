875a3622af40e8943fa9992f69ebf45c3b682b16
Itay Hadad <itay5245@gmail.com>
2024-02-03 21:08:41 +0200

    comment

--- Headers/Utils/Functions/print.hpp
+++ Headers/Utils/Functions/print.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-// future me: create a class for each Utils/Functions and implement it in cpp
+// future me: create a class for each Utils/Functions and implement it in cpp.
+// after that implement missing-implementation hpp files
 class PrintUtils
 {
 public:

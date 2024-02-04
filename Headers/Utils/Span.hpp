cfa6b2708c9b3990a01c7a3b7259aa64e339afd3
Itay Hadad <itay5245@gmail.com>
2024-02-05 01:12:38 +0200

    implemented `Span` class

--- Headers/Utils/Span.hpp
+++ Headers/Utils/Span.hpp
@@ -2,7 +2,9 @@
 
 #include <cstdint>
 #include <type_traits>
+
 #include "KernelException.hpp"
+#include "ThirdParty/Types/stddef.h"
 
 template<typename T>
 class Span
@@ -11,84 +13,30 @@ public:
 	constexpr Span(): _ptr(nullptr), _count(0)
 	{}
 
-	constexpr explicit Span(T* ptr, uint32_t count) : _ptr(ptr), _count(count)
-	{
-		if ((ptr == nullptr) && (count != 0))
-		{
-			THROW_KERNEL_EXCEPTION();
-		}
-	}
+	constexpr explicit Span(T* ptr, uint32_t count);
 
 public:
-	constexpr std::add_const_t<T>* begin() const
-	{
-		return _ptr;
-	}
+	constexpr uint32_t size();
 
-	constexpr std::add_const_t<T>* end() const
-	{
-		return _ptr + _count;
-	}
+	constexpr T* begin();
+	constexpr std::add_const_t<T>* begin() const;
 
-	constexpr T* begin()
-	{
-		return _ptr;
-	}
-
-	constexpr T* end()
-	{
-		return _ptr + _count;
-	}
-
-	constexpr uint32_t size()
-	{
-		return _count;
-	}
+	constexpr T* end();
+	constexpr std::add_const_t<T>* end() const;
 
 	template <typename U>
-	constexpr Span<T> copy_from_impl(const Span<U>& span_to_copy, size_t size)
-	{
-		if (this == &span_to_copy) {
-			return *this;
-		}
+	constexpr Span<T> copy_from_impl(const Span<U>& span_to_copy, size_t size);
 
-		static_assert(size == this->_count, "Size mismatch in copy_from");
-		for (int i = 0; i < this->_count; ++i) {
-			this->_ptr[i] = span_to_copy[i];
-		}
+	constexpr Span<T> copy_from(const Span<T>& span_to_copy, size_t size);
 
-		return *this;
-	}
+	constexpr Span<T> copy_from(const Span<std::add_const_t<T>>& span_to_copy, size_t size);
 
-	constexpr Span<T> copy_from(const Span<T>& span_to_copy, size_t size)
-	{
-		return copy_from_impl(span_to_copy, size);
-	}
-
-	constexpr Span<T> copy_from(const Span<std::add_const_t<T>>& span_to_copy, size_t size)
-	{
-		return copy_from_impl(span_to_copy, size);
-	}
-
-	constexpr T& operator[](uint32_t index)
-	{
-		if (index >= _count)
-		{
-			THROW_KERNEL_EXCEPTION();
-		}
-
-		return _ptr[index];
-	}
+	constexpr T& operator[](uint32_t index);
 
 	constexpr std::add_const_t<T>& operator[](uint32_t index) const
-	{
-		if (index >= _count)
-		{
-			THROW_KERNEL_EXCEPTION();
-		}
 
-		return _ptr[index];
-	}
+private:
+	constexpr Span<T> copy_from_impl(const Span<U>& span_to_copy, size_t size);
 
 protected:
 	T* _ptr;

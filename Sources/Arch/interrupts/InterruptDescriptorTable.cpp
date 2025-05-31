2c2fe64d9f6875d0473db47468198f4281b61d7c
Itay Hadad <itay5245@gmail.com>
2025-05-31 15:34:04 +0300

    indentation

--- Sources/Arch/interrupts/InterruptDescriptorTable.cpp
+++ Sources/Arch/interrupts/InterruptDescriptorTable.cpp
@@ -6,111 +6,110 @@
 #include "Arch/InterruptDescriptorTable.hpp"
 #include "Arch/InterruptServiceRoutine/InterruptServiceRoutineInitializer.hpp"
 
-ProcessorRegisterSet* isr_function_handler(ProcessorRegisterSet* registers, InterruptServiceRoutineEntries* isr_entries)
+ProcessorRegisterSet *isr_function_handler(ProcessorRegisterSet *registers, InterruptServiceRoutineEntries *isr_entries)
 {
-	constexpr int NO_INTERRUPT_HANDLER = 0;
-    const IsrEntry_t* isr_entry = &(isr_entries->get_isr_entries()[registers->interrupt_number]);
+    constexpr int NO_INTERRUPT_HANDLER = 0;
+    const IsrEntry_t *isr_entry = &(isr_entries->get_isr_entries()[registers->interrupt_number]);
 
-	if (isr_entry == NULL)
-	{
-		PrintUtils::printk("no function to handle interrupt. ");
-		THROW_KERNEL_EXCEPTION();
-	}
-	else
-	{
-		isr_entry->second()(registers);
-	}
+    if (isr_entry == NULL)
+    {
+        PrintUtils::printk("no function to handle interrupt. ");
+        THROW_KERNEL_EXCEPTION();
+    } else
+    {
+        isr_entry->second()(registers);
+    }
 
-	return registers;
+    return registers;
 }
 
-void InterruptDescriptorTable::idt_set_entry(uint8_t entry_number, IsrFunction_t* target_function)
+void InterruptDescriptorTable::idt_set_entry(uint8_t entry_number, IsrFunction_t *target_function)
 {
-	uint64_t funcAddr = reinterpret_cast<uint64_t>(target_function);
-	IdtEntry* entry = &this->_entries[0];
+    uint64_t funcAddr = reinterpret_cast<uint64_t>(target_function);
+    IdtEntry *entry = &this->_entries[entry_number];
 
-	entry->isr_low = funcAddr & 0XFFFF;
-	entry->isr_mid = (funcAddr >> 16) & 0xFFFF;
-	entry->isr_high = (funcAddr >> 32) & 0XFFFFFFFF;
+    entry->isr_low = funcAddr & 0XFFFF;
+    entry->isr_mid = (funcAddr >> 16) & 0xFFFF;
+    entry->isr_high = (funcAddr >> 32) & 0XFFFFFFFF;
 
-	entry->type_attributes = 0xEF;
-	entry->ist_index = 0;
-	entry->selector = 8; // CODE descriptor, see gdt64.code
+    entry->type_attributes = 0xEF;
+    entry->ist_index = 0;
+    entry->selector = 8; // CODE descriptor, see gdt64.code
 }
 
 void InterruptDescriptorTable::set_all_idt_entries()
 {
-	// GENERAL CPU INTERRUPTS
-	idt_set_entry(0, isr0);
-	idt_set_entry(1, isr1);
-	idt_set_entry(2, isr2);
-	idt_set_entry(3, isr3);
-	idt_set_entry(4, isr4);
-	idt_set_entry(5, isr5);
-	idt_set_entry(6, isr6);
-	idt_set_entry(7, isr7);
-	idt_set_entry(8, isr8);
-	idt_set_entry(9, isr9);
-	idt_set_entry(10, isr10);
-	idt_set_entry(11, isr11);
-	idt_set_entry(12, isr12);
-	idt_set_entry(13, isr13);
-	idt_set_entry(14, isr14);
-	idt_set_entry(15, isr15);
-	idt_set_entry(16, isr16);
-	idt_set_entry(17, isr17);
-	idt_set_entry(18, isr18);
-	idt_set_entry(19, isr19);
-	idt_set_entry(20, isr20);
-	idt_set_entry(21, isr21);
-	idt_set_entry(22, isr22);
-	idt_set_entry(23, isr23);
-	idt_set_entry(24, isr24);
-	idt_set_entry(25, isr25);
-	idt_set_entry(26, isr26);
-	idt_set_entry(27, isr27);
-	idt_set_entry(28, isr28);
-	idt_set_entry(29, isr29);
-	idt_set_entry(30, isr30);
-	idt_set_entry(31, isr31);
-	idt_set_entry(32, isr32);
-	idt_set_entry(33, isr33);
-	idt_set_entry(34, isr34);
-	idt_set_entry(35, isr35);
-	idt_set_entry(36, isr36);
-	idt_set_entry(37, isr37);
-	idt_set_entry(38, isr38);
-	idt_set_entry(39, isr39);
-	idt_set_entry(40, isr40);
-	idt_set_entry(41, isr41);
-	idt_set_entry(42, isr42);
-	idt_set_entry(43, isr43);
-	idt_set_entry(44, isr44);
-	idt_set_entry(45, isr45);
-	idt_set_entry(46, isr46);
-	idt_set_entry(47, isr47);
+    // GENERAL CPU INTERRUPTS
+    idt_set_entry(0, isr0);
+    idt_set_entry(1, isr1);
+    idt_set_entry(2, isr2);
+    idt_set_entry(3, isr3);
+    idt_set_entry(4, isr4);
+    idt_set_entry(5, isr5);
+    idt_set_entry(6, isr6);
+    idt_set_entry(7, isr7);
+    idt_set_entry(8, isr8);
+    idt_set_entry(9, isr9);
+    idt_set_entry(10, isr10);
+    idt_set_entry(11, isr11);
+    idt_set_entry(12, isr12);
+    idt_set_entry(13, isr13);
+    idt_set_entry(14, isr14);
+    idt_set_entry(15, isr15);
+    idt_set_entry(16, isr16);
+    idt_set_entry(17, isr17);
+    idt_set_entry(18, isr18);
+    idt_set_entry(19, isr19);
+    idt_set_entry(20, isr20);
+    idt_set_entry(21, isr21);
+    idt_set_entry(22, isr22);
+    idt_set_entry(23, isr23);
+    idt_set_entry(24, isr24);
+    idt_set_entry(25, isr25);
+    idt_set_entry(26, isr26);
+    idt_set_entry(27, isr27);
+    idt_set_entry(28, isr28);
+    idt_set_entry(29, isr29);
+    idt_set_entry(30, isr30);
+    idt_set_entry(31, isr31);
+    idt_set_entry(32, isr32);
+    idt_set_entry(33, isr33);
+    idt_set_entry(34, isr34);
+    idt_set_entry(35, isr35);
+    idt_set_entry(36, isr36);
+    idt_set_entry(37, isr37);
+    idt_set_entry(38, isr38);
+    idt_set_entry(39, isr39);
+    idt_set_entry(40, isr40);
+    idt_set_entry(41, isr41);
+    idt_set_entry(42, isr42);
+    idt_set_entry(43, isr43);
+    idt_set_entry(44, isr44);
+    idt_set_entry(45, isr45);
+    idt_set_entry(46, isr46);
+    idt_set_entry(47, isr47);
 }
 
 InterruptDescriptorTable::InterruptDescriptorTable()
 {
-	IdtEntry entries_array[this->IDT_SIZE];
-	MemoryUtils::memset(entries_array, NULL, this->IDT_SIZE);
+    IdtEntry entries_array[this->IDT_SIZE];
+    MemoryUtils::memset(entries_array, NULL, this->IDT_SIZE);
 
-	this->_entries = Span<IdtEntry>(entries_array, this->IDT_SIZE);
-	this->_idt_descriptor->addr = this->_entries[0];
-	this->_idt_descriptor->size = sizeof(this->_entries) - 1;
+    this->_entries = Span<IdtEntry>(entries_array, this->IDT_SIZE);
+    this->_idt_descriptor->addr = this->_entries[0];
+    this->_idt_descriptor->size = sizeof(this->_entries) - 1;
 
-	this->set_all_idt_entries();
+    this->set_all_idt_entries();
 
-	load_idt(this->_idt_descriptor);
+    load_idt(this->_idt_descriptor);
 }
 
 Span<IdtEntry> InterruptDescriptorTable::getEntries()
 {
-	return this->_entries;
+    return this->_entries;
 };
 
-IdtDescriptor* InterruptDescriptorTable::getIdtDescriptor()
+IdtDescriptor *InterruptDescriptorTable::getIdtDescriptor()
 {
-	return this->_idt_descriptor;
+    return this->_idt_descriptor;
 };

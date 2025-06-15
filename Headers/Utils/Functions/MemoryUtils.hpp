cc3a44e5e564ece2bbc567b4c3f0eba0101fe505
Itay Hadad <itay5245@gmail.com>
2025-06-15 13:57:02 +0300

    refactor memset

--- Headers/Utils/Functions/MemoryUtils.hpp
+++ Headers/Utils/Functions/MemoryUtils.hpp
@@ -3,17 +3,11 @@
 
 namespace MemoryUtils
 {
-    template <typename T>
-    constexpr T* memset(T *ptr, char c, size_t size)
-    {
-        unsigned char* byte_ptr = (unsigned char *)ptr;
-        unsigned char byte_value = (unsigned char)c;
-
-        for(unsigned int i = 0; i < size; i++)
-        {
-            byte_ptr[i] = byte_value;
+    void* memset(void* ptr, int value, size_t num) {
+        unsigned char* p = static_cast<unsigned char*>(ptr);
+        for (size_t i = 0; i < num; ++i) {
+            p[i] = static_cast<unsigned char>(value);
         }
-
         return ptr;
-    };
+    }
 };

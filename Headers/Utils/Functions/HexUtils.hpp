45dc76fbe1d865617ef3332da72bf1450e4979ea
Itay Hadad <itay5245@gmail.com>
2025-06-17 22:36:21 +0300

    added extract_nibble utility

--- Headers/Utils/Functions/HexUtils.hpp
+++ Headers/Utils/Functions/HexUtils.hpp
@@ -4,7 +4,7 @@
 
 namespace HexUtils
 {
-    static constexpr unsigned int extract_nibble(uint32_t value, int index_from_left)
+    static constexpr unsigned int extract_nibble(int value, int index_from_left)
     {
         return (value >> (28 - (index_from_left * 4))) & 0xF;
     }

3076bd12564bae584111b4e9560bc819d3967cc9
Itay Hadad <itay5245@gmail.com>
2025-06-20 10:26:45 +0300

    removed static from HexUtils.hpp

--- Headers/Utils/Functions/HexUtils.hpp
+++ Headers/Utils/Functions/HexUtils.hpp
@@ -4,7 +4,7 @@
 
 namespace HexUtils
 {
-    static constexpr unsigned int extract_nibble(int value, int index_from_left)
+    constexpr unsigned int extract_nibble(int value, int index_from_left)
     {
         return (value >> (28 - (index_from_left * 4))) & 0xF;
     }
